31c89f47ad17cfe3e96fceb9ac0461fb7fb1766b
gaultpay000 <pgauptney@gmail.com>
2023-03-01 20:21:34 -0600

    Auto implimentation start

---
+++ src/main/java/frc/robot/commands/auto/paths/AutoPath.java
@@ -0,0 +1,18 @@
+package frc.robot.commands.auto.paths;
+
+import com.pathplanner.lib.commands.PPRamseteCommand;
+
+import frc.robot.subsystems.DriveSubsystem;
+
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+public class AutoPath extends PPRamseteCommand{
+    PathPlannerTrajectory trajectory;
+    DriveSubsystem driveSubsystem;
+
+    public AutoPath(PathPlannerTrajectory trajectory, DriveSubsystem driveSubsystem) {
+        super
+
+
+    }
+}
--- src/main/java/frc/robot/commands/auto/paths/ScoreDock.java
+++ src/main/java/frc/robot/commands/auto/paths/ScoreDock.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.paths;
+package frc.robot.commands.auto.paths;
 
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -8,21 +8,18 @@ import frc.robot.subsystems.ClawSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExtenderSubsystem;
 import frc.robot.commands.Extender;
+import com.pathplanner.lib.*;
 
 public class ScoreDock extends SequentialCommandGroup {
     public ScoreDock(DriveSubsystem driveSubsystem, ClawSubsystem clawSubsystem, ExtenderSubsystem extenderSubsystem){
-        Trajectory trajectory1 = driveSubsystem.loadTrajectoryFromFile("ScoreDock");
+        PathPlannerTrajectory ScoreDock = PathPlanner.loadPath("ScoreDock", new PathConstraints(4, 3));
 
         addCommands(
             new InstantCommand(() -> {
                 driveSubsystem.resetOdometry();
             }),
-            new InstantCommand(() -> {
-                extenderSubsystem.extend(); //TODO: Unless fixed, this will extend and retract on repeat.
-            }),
-            new InstantCommand(() -> {
-                clawSubsystem.grab(); //Same with this
-            })
+            new Extender(extenderSubsystem),
+            new Claw(clawSubsystem)
         );
     }
 }
