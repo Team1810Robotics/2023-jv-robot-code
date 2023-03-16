a73fe2d88f6040ca19d69cf0f251dcea0c704ba2
Tlesis <jackwilliamson.1215@gmail.com>
2023-03-16 18:03:22 -0500

    Moving over to JV dsl

--- src/main/java/frc/robot/commands/auto/paths/ScoreDock.java
+++ src/main/java/frc/robot/commands/auto/paths/ScoreDock.java
@@ -1,26 +1,32 @@
-/* package frc.robot.commands.auto.paths;
+package frc.robot.commands.auto.paths;
 
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.Claw;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.commands.Autonomous;
 import frc.robot.subsystems.ClawSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExtenderSubsystem;
-import frc.robot.commands.Extender;
-import com.pathplanner.lib.*;
 
-public class ScoreDock extends SequentialCommandGroup {
-    public ScoreDock(DriveSubsystem driveSubsystem, ClawSubsystem clawSubsystem, ExtenderSubsystem extenderSubsystem){
-        PathPlannerTrajectory ScoreDock = PathPlanner.loadPath("ScoreDock", new PathConstraints(4, 3));
+public class ScoreDock extends CommandBase {
 
-        addCommands(
-            new InstantCommand(() -> {
-                driveSubsystem.resetOdometry();
-            }),
-            new Extender(extenderSubsystem),
-            new Claw(clawSubsystem)
-        );
+    private DriveSubsystem drive;
+    private ClawSubsystem claw;
+    private ExtenderSubsystem extender;
+
+    private Autonomous dock;
+
+    public ScoreDock(DriveSubsystem driveSubsystem, ClawSubsystem clawSubsystem, ExtenderSubsystem extenderSubsystem) {
+        this.drive = driveSubsystem;
+        this.claw = clawSubsystem;
+        this.extender = extenderSubsystem;
+
+        this.dock = new Autonomous();
+
+        addRequirements(driveSubsystem, clawSubsystem, extenderSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        var speed = dock.autoDockRoutine();
+        drive.drive(-speed, -speed);
     }
 }
- */
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,16 +1,5 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.sensors.PigeonIMU;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryUtil;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Filesystem;
-//import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
@@ -18,15 +7,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.*;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-
 public class DriveSubsystem extends SubsystemBase {
-
-
-    private static final PigeonIMU pigeon = new PigeonIMU(DriveConstants.PIGEON);
-
-    private final DifferentialDriveOdometry odometer;
 
     private PWMSparkMax frontLeftMotor;
     private PWMSparkMax backLeftMotor;
@@ -38,10 +19,6 @@ public class DriveSubsystem extends SubsystemBase {
     private MotorControllerGroup leftDrive;
 
     private DifferentialDrive differentialDrive;
-
-    private Encoder leftEncoder;
-
-    private Encoder rightEncoder;
 
     public DriveSubsystem() {
         frontLeftMotor = new PWMSparkMax(DriveConstants.FRONT_LEFT_MOTOR_ID);
@@ -57,49 +34,14 @@ public class DriveSubsystem extends SubsystemBase {
         rightDrive.setInverted(DriveConstants.RIGHT_INVERTED);
 
         differentialDrive = new DifferentialDrive(leftDrive, rightDrive);
-
-        leftEncoder = new Encoder(2, 3);
-
-        rightEncoder = new Encoder(8, 9);
-
-        pigeon.setFusedHeading(0.0);
-
-        odometer = new DifferentialDriveOdometry(new Rotation2d(), 0, 0);
-
     }
 
     public void drive(double leftSpeed, double rightSpeed) {
         differentialDrive.tankDrive(leftSpeed, rightSpeed, true);
     }
 
-    public Trajectory loadTrajectoryFromFile(String filename) {
-        try {
-            return loadTrajectory(filename);
-        } catch (IOException e) {
-            DriverStation.reportError("Failed to load auto trajectory: " + filename, false);
-            return new Trajectory();
-        }
-    }
-
-    public Trajectory loadTrajectory(String trajectoryName) throws IOException {
-        return TrajectoryUtil.fromPathweaverJson(Filesystem.getDeployDirectory().toPath().resolve(Paths.get("paths", trajectoryName + ".wpilib.json")));
-    }
-
-    public Pose2d getPose() {
-        return odometer.getPoseMeters();
-    }
-    
-    public void resetOdometry() {
-        odometer.update(new Rotation2d(), 0, 0);
-    }
-
     public void stop() {
         leftDrive.set(0);
         rightDrive.set(0);
-    }
-
-    @Override
-    public void periodic() {
-        odometer.update(Rotation2d.fromDegrees(pigeon.getYaw()), leftEncoder.getDistance(), rightEncoder.getDistance());
     }
 }
