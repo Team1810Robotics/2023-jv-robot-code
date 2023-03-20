494476bdf53d8fdb020353d8069320d970483a96
Noah Johnson <johnsnoa001@usd232.org>
2023-03-20 08:36:43 -0500

    Forgot i commented out some of the auto

--- src/main/java/frc/robot/commands/auto/Score.java
+++ src/main/java/frc/robot/commands/auto/Score.java
@@ -1,6 +1,9 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.Claw;
+import frc.robot.commands.Extender;
 import frc.robot.subsystems.ClawSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExtenderSubsystem;
@@ -8,15 +11,16 @@ import frc.robot.subsystems.ExtenderSubsystem;
 public class Score extends SequentialCommandGroup {
     public Score(ExtenderSubsystem extenderSubsystem, ClawSubsystem clawSubsystem, DriveSubsystem driveSubsystem) {
                     // drop cube
-        addCommands(/* new Extender(extenderSubsystem),
+        addCommands(new Extender(extenderSubsystem),
                     new Claw(clawSubsystem),
-                    new WaitCommand(0.5), */
+                    new WaitCommand(0.5),
 
                     // drive back
                     new Drive(-0.25, -0.25, driveSubsystem, 5),
-                    new Drive(0, 0, driveSubsystem, 0.5)/* ,
+                    new Drive(0, 0, driveSubsystem, 0.5),
+
                     // reset claw
                     new Claw(clawSubsystem),
-                    new Extender(extenderSubsystem) */);
+                    new Extender(extenderSubsystem));
     }
 }
